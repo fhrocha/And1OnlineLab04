89516254a837aec98ee84faa047a3aecb903f355
Fabio <fhrocha@kr-itsolutions.com.br>
2012-11-20 18:08:34 -0200

    Finish work

--- src/br/com/globalcode/android/MainActivity.java
+++ src/br/com/globalcode/android/MainActivity.java
@@ -1,8 +1,12 @@
 package br.com.globalcode.android;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -17,4 +21,36 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
     }
+    
+    public void clearFields(View view) {
+
+		final EditText editTextName = (EditText)this.findViewById(R.id.editTextName);
+        final EditText editTextPassword = (EditText)this.findViewById(R.id.editTextPass);
+        final TextView textViewStatus = (TextView)this.findViewById(R.id.txtViewStatus);
+        
+        editTextName.setText("");
+        editTextPassword.setText("");
+        textViewStatus.setText("");
+        
+        editTextName.requestFocus();
+    }
+    
+    public void checkLogin(View view) {
+    	
+		final EditText editTextName = (EditText)this.findViewById(R.id.editTextName);
+        final EditText editTextPassword = (EditText)this.findViewById(R.id.editTextPass);
+        final TextView textViewStatus = (TextView)this.findViewById(R.id.txtViewStatus);
+        
+        if( "Fábio".equalsIgnoreCase(editTextName.getText().toString()) &&
+        		"123".equals(editTextPassword.getText().toString()) ) {
+        	
+        	textViewStatus.setText("Login OK!");
+        	textViewStatus.setTextColor(Color.BLACK);
+        } else {
+        	
+        	textViewStatus.setText("Access Denied");
+        	textViewStatus.setTextColor(Color.RED);
+        }
+    }
+
 }
